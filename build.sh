b2cbfe539d6e7da7007524588e1874c3031c0b21
Zev <zevvez@foxmail.com>
2018-05-08 12:31:45 +0000

    auto change play-mqtt-daemon.service

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+EXECPATH=$PWD
 sudo apt update && sudo apt-get install build-essential gcc make cmake git libssl-dev libjpeg8-dev libev-dev linux-tools-common linux-tools-generic libncurses5-dev -y
 
 # build paho.mqtt.c
@@ -9,7 +10,7 @@ cmake -DPAHO_WITH_SSL=TRUE ..
 make && sudo make install
  
 # build paho.mqtt.cpp
-cd ../../
+cd $EXECPATH
 git clone https://github.com/eclipse/paho.mqtt.cpp.git
 cd paho.mqtt.cpp/
 mkdir build && cd build
@@ -19,23 +20,24 @@ sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf'
 sudo ldconfig -v
  
 # build mjpg-streamer
-cd ../../
+cd $EXECPATH
 git clone https://github.com/jacksonliam/mjpg-streamer.git
 cd mjpg-streamer/mjpg-streamer-experimental/
 make
 
 # build lepd
-cd ../../../
+cd $EXECPATH
 git clone https://github.com/ZevveZ/lepd.git
 cd lepd/
 make
 
 # build controller
-cd ../controller/
+cd $EXECPATH/controller/
 make
 
 # configure systemd
-cd ../
+cd $EXECPATH
+sed -i "6s#path#$EXECPATH#" play-mqtt-daemon.service
 sudo cp play-mqtt-daemon.service /lib/systemd/system/
 sudo systemctl enable play-mqtt-daemon
 sudo systemctl start play-mqtt-daemon
